38091ce6b014badcc270013cecb7f863003c9a3d
EliseoAlvarezB <ealvarezbrescia@mail.austral.edu.ar>
2025-08-25 12:11:22 -0300

    Add files via upload

---
+++ tp3-EliseoAlvarezB-main/slice_simple.py
@@ -0,0 +1,14 @@
+def slice_simple():
+    texto = "Awesome"
+    mins = texto.lower()
+    print(mins[0:3])
+    print(mins[2:5])
+    print(mins)
+
+
+    # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
+    # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
+ 
+slice_simple()
+# Para verificar este ejercicio ejecutar el comando
+# `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
