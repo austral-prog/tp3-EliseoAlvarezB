c586de6221f583c0d8329680f08d502cd1055960
iosephj <juarez.jose.g@gmail.com>
2025-11-24 15:02:11 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,21 +1,8 @@
 def check_vowels():
 
     name_1 = input("Ingrese un nombre: ")
-    print("")
-
     print("Contiene a:","a" in name_1.lower())
     print("Contiene e:","e" in name_1.lower())
     print("Contiene i:","i" in name_1.lower())  
     print("Contiene o:","o" in name_1.lower())
     print("Contiene u:","u" in name_1.lower())
-
-    print("")
-
-    name_2 = input("Ingrese un nombre: ")
-    print("")
-
-    print("Contiene a:","a" in name_2.lower())
-    print("Contiene e:","e" in name_2.lower())
-    print("Contiene i:","i" in name_2.lower())
-    print("Contiene o:","o" in name_2.lower())
-    print("Contiene u:","u" in name_2.lower())
